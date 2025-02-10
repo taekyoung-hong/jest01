80f9d5def49926e38cf546c8671643896dbb00ee
Taekyoung <>
2025-02-10 21:12:17 +0900

    Material UI Library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { Typography } from '@mui/material';
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>      
-    </div>
+    <Typography variant='h2' component='h2' data-test='heading'>
+      Bookish      
+    </Typography>
   );
 }
 
